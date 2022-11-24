95ab8c676421d5ee0d9229200c176af3fcb9c764
Niko <niko0728.it@gmail.com>
2022-11-24 17:12:23 +1100

    Compeleted

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express")
 var app = express()
+var port = process.env.port || 3000;
 
 app.use(express.static(__dirname + '/public'))
 app.use(express.json())
@@ -20,7 +21,7 @@ app.get("/addTwoNumbers", (req, res) => {
     res.json({ statusCode: 200, data: result, message: "Success" })
 })
 
-var port = process.env.port || 3000;
+
 
 app.listen(port, () => {
     console.log("App listening to: http://localhost:" + port)
